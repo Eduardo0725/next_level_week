be569961431e52c03dd03c9090f4732bc6f34291
Eduardo0725 <eduardom0142@gmail.com>
2020-06-06 15:02:56 -0300

    A escolha da stack

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -43,3 +43,45 @@ document
     .querySelector("select[name=uf]")
     .addEventListener("change", getCities)
 ;
+
+//Itens de coleta
+const itemsToCollect = document.querySelectorAll(".items-grid li");
+
+for ( const item of itemsToCollect ) {
+    item.addEventListener('click', handleSelectedItem)
+}
+
+const collectedItems = document.querySelector("input[name=items]");
+
+let selectedItems = [];
+
+function handleSelectedItem(event){
+    const itemLi = event.target;
+
+    // adicionar ou remover uma classe com javascript
+    itemLi.classList.toggle("selected");
+
+    const itemId = event.target.dataset.id;
+
+    // verificar se existem itens selecionados, se sim
+    // pegar os itens selecionados
+    const alreadySelected = selectedItems.findIndex( item => item == itemId );
+
+    //se já estiver selecionado
+    if(alreadySelected >= 0){
+        //tirar da seleção
+        const filteredItems = selectedItems.filter(item => {
+            const itemIsDifferent = item != itemId;
+            return itemIsDifferent;
+        });
+
+        selectedItems = filteredItems;
+    }else{
+        // se não estiver selecionado
+        // adicionar à seleção
+        selectedItems.push(itemId);
+    }
+
+    // atualizar o campo escondido com os itens selecionados
+    collectedItems.value = selectedItems;
+}
