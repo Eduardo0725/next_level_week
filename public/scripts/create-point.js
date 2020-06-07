31c262df06a9a80a48cc25d665e5a4e2ba69f8c0
Eduardo0725 <eduardom0142@gmail.com>
2020-06-07 00:28:06 -0300

    A milha extra

--- public/scripts/create-point.js
+++ public/scripts/create-point.js
@@ -63,6 +63,8 @@ function handleSelectedItem(event){
 
     const itemId = event.target.dataset.id;
 
+    // console.log("ITEM ID: ", itemId);
+
     // verificar se existem itens selecionados, se sim
     // pegar os itens selecionados
     const alreadySelected = selectedItems.findIndex( item => item == itemId );
@@ -81,6 +83,8 @@ function handleSelectedItem(event){
         // adicionar à seleção
         selectedItems.push(itemId);
     }
+
+    // console.log("selectedItems: ", selectedItems);
 
     // atualizar o campo escondido com os itens selecionados
     collectedItems.value = selectedItems;
